ed6c948a7d3115640a12da154d351596b69bf58a
Gerry Huang <gerryhzgtest2021@yahoo.com>
2021-08-02 19:58:52 -0700

    delete the modules directory

--- modules/elb_auto/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "ec2_public_key" {
-  value = jsondecode(data.aws_secretsmanager_secret_version.ec2_public_key.secret_string)["public_key"]
-}
-
-output "example-instance-security-group-id" {
-  value = aws_security_group.example-instance.id
-}
--- modules/rds/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "db_password" {
-  value = local.db-password
-}
-output "db_endpoint" {
-  value = aws_db_instance.mysqldb.endpoint
-}
-output "db_address" {
-  value = aws_db_instance.mysqldb.address
-}
